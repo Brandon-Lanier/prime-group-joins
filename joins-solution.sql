9835c8c920bffcc4c00f4b9f89d8693387c073c5
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-01 11:45:53 -0600

    Solved question 5

--- joins-solution.sql
+++ joins-solution.sql
@@ -6,3 +6,20 @@ JOIN "addresses" ON "addresses"."customer_id" = "customers"."id";
 SELECT * FROM "orders"
 JOIN "line_items" ON "line_items"."order_id" = "orders"."id";
 
+-- Which warehouses have cheetos?
+SELECT "warehouse"."warehouse" FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = "warehouse"."id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'cheetos';
+
+-- Which warehouses have diet pepsi?
+SELECT "warehouse"."warehouse" FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = "warehouse"."id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
+
+-- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT "customers", count("orders"."id") AS "order count" FROM "customers"
+JOIN "addresses" ON "addresses"."customer_id" = "customers"."id"
+JOIN "orders" ON "orders"."address_id" = "addresses"."id"
+GROUP BY "customers";
